97c3862d78e507ffa8624cdd0c81fe27f884f4d0
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-08-17 21:21:35 +0600

    calculate triangle & rectangle Area

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -8,4 +8,29 @@ function calculateTriangleArea(){
     const heightField = document.getElementById('triangle-height');
     const heightValueText = heightField.value;
     const height = parseFloat(heightValueText);
+
+    const area = 0.5 * base * height;
+
+    // show triangle area 
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+}
+
+function calculateRectangleArea(){
+    // get rectangle width 
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+
+    // get recetangle length 
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    
+    // claculate area 
+    const area = width * length;
+
+    // show rectangle area 
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
